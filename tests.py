015cc853301657a639c9993aff10c59ed4692a9f
epopisces <epopisces@gmail.com>
2022-03-03 12:20:09 -0600

    develop template tests, placeholder vars further

--- tests.py
+++ tests.py
@@ -1,7 +1,20 @@
-# test code is a good idea!
-#import os
+# testing code is a good idea!
+#import os #? for os.getenv('ENVNAME')
 
 import toolname_api as shortname
 
-def test_case_1():
+#region #####=-   Configure Tests                                            -=###########
+def test_case_1(shortname):
+    shortname.function('test param')
     return
+
+#endregion
+
+shortname = shortname.ObjectClass()
+
+shortname
+
+#region #####=-   Execute Tests                                                -=#########
+test_case_1(shortname)
+
+#endregion
